b40ff1f16df0881d12468b658a5c8ed8cb85aab7
RoyMamoru <m7415369n.go@gmail.com>
2018-12-22 11:12:39 +0900

    add input_form

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import InputForm
+from .models import Customers
 
 def index(request):
     return render(request, 'demo_app/index.html', {})
+
+def input_form(request):
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form.save() #  入力された値を保存
+            return redirect('result')
+
+    else:
+        form = InputForm()
+        return render(request, 'demo_app/input_form.html', {'form':form})
+
+def result(request):
+    return render(request, 'demo_app/result.html', {})
